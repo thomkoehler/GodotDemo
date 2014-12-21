bf66e5709a57257934d6587bcc6fb80153e851c3
thom.koehler <thom.koehler@gmail.com>
2014-12-21 14:57:10 +0100

    GodotDemo

--- Pong.gd
+++ Pong.gd
@@ -1,24 +1,49 @@
 
-extends Area2D
+extends Node2D
 
-var screen_size
-var pad_size
-var ball_speed = 80
+var screenSize
+var padSize
+var ballSpeed = 80
 var direction = Vector2(-1,0)
+var outLabel
 const PAD_SPEED = 250
 
 
 func _ready():
-    screen_size = get_viewport_rect().size
-    pad_size = get_node("left").get_texture().get_size()
-    set_process(true)
+	outLabel = get_node("out")
+	screenSize = get_viewport_rect().size
+	padSize = get_node("left").get_texture().get_size()
+	set_process(true)
+	# outLabel.set_text("Hallo")
 
 
 func _process(delta):
-	var left_pos = get_node("left").get_pos()
-	if (left_pos.y > 0 and Input.is_action_pressed("leftMoveUp")):
-		left_pos.y+=-PAD_SPEED*delta
-	if (left_pos.y < screen_size.y and Input.is_action_pressed("leftMoveDown")):
-        left_pos.y+=PAD_SPEED*delta
+	moveLeftPad(delta)
+	moveRightPad(delta)
 
-	get_node("left").set_pos(left_pos)
+
+func moveLeftPad(delta):
+	var leftPos = get_node("left").get_pos()
+	
+	if (leftPos.y > 0 and Input.is_action_pressed("leftMoveUp")):
+		leftPos.y -= PAD_SPEED*delta
+		get_node("left").set_pos(leftPos)
+		
+	if (leftPos.y < screenSize.y and Input.is_action_pressed("leftMoveDown")):
+        leftPos.y += PAD_SPEED*delta
+        get_node("left").set_pos(leftPos)
+
+
+func moveRightPad(delta):
+	var rightPos = get_node("right").get_pos()
+	
+	if rightPos.y > 0 and Input.is_action_pressed("rightMoveUp"):
+		rightPos.y -= PAD_SPEED*delta
+		get_node("right").set_pos(rightPos)
+		
+	if rightPos.y < screenSize.y and Input.is_action_pressed("rightMoveDown"):
+        rightPos.y += PAD_SPEED*delta
+        get_node("right").set_pos(rightPos)
+
+
+	
